fd0d24da764423bbfa275f32beb91ef75a562972
Aakash Shukla <shukla.aa@northeastern.edu>
2022-10-18 02:27:18 -0400

    Added factory pattern

---
+++ FactoryMethod/FactoryMethod/Creator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    // The Creator class declares the factory method that is supposed to return
+    // an object of a Product class. The Creator's subclasses usually provide
+    // the implementation of this method.
+    public abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+
+            var result = "Creator: The same creator's code worked with " + product.Operation();
+
+            return result;
+        }
+
+    }
+}
