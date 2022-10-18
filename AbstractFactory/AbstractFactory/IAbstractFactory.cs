08a86447b8f04f07bb5e3275262d7b907d9e9a37
Aakash Shukla <shukla.aa@northeastern.edu>
2022-10-18 03:21:31 -0400

    Added abstract factory

---
+++ AbstractFactory/AbstractFactory/IAbstractFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    // Products of one family are usually able to collaborate among themselves.
+    // A family of products may have several variants, but the products
+    // of one variant are incompatible with products of another.
+    public interface IAbstractFactory
+    {
+        IAbstractProductA CreateProductA();
+        IAbstractProductB CreateProductB();
+    }
+}
